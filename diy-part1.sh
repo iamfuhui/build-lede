36edfc19a68423025b047092befcf7088fe9ae2b
iamfuhui <48637642+iamfuhui@users.noreply.github.com>
2021-02-14 09:22:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,7 +8,22 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
+
+# 配置自定义主题
 #
+# > 清除旧版argon主题并拉取最新版
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/community/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/community/luci-app-argon-config
+#
+# > 修改默认主题为 Argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#
+# > AdguardHome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/community/luci-app-adguardhome
+# > HelloWorld (vssr)
+git clone https://github.com/jerrykuku/lua-maxminddb package/community/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr package/community/luci-app-vssr
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
